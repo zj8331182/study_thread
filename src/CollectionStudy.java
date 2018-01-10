cfd34eb8b77c6c9d2d89ca33c5451daa967850f9
张明哲 <1084904209@qq.com>
2018-01-10 15:14:12 +0800

    添加集合的简单学习

---
+++ src/CollectionStudy.java
@@ -0,0 +1,223 @@
+import java.util.*;
+
+public class CollectionStudy {
+
+    private final static int Count = 100000;
+
+    public static void main(String[] args) {
+        List<String> arrList = new ArrayList<>();
+        List<String> linkedList = new LinkedList<>();
+        String a;
+
+        long timeArr = System.currentTimeMillis();
+        for (int i = 0; i < Count; i++) {
+            arrList.add("" + i);
+        }
+        System.out.println(System.currentTimeMillis() - timeArr);
+
+        timeArr = System.currentTimeMillis();
+        for (int i = 0; i < Count; i++) {
+            a = arrList.get(i);
+        }
+        System.out.println(System.currentTimeMillis() - timeArr);
+
+        long timeLink = System.currentTimeMillis();
+        for (int i = 0; i < Count; i++) {
+            linkedList.add("" + i);
+        }
+        System.out.println(System.currentTimeMillis() - timeLink);
+
+
+        timeLink = System.currentTimeMillis();
+        for (int i = 0; i < Count; i++) {
+            a = linkedList.get(i);
+        }
+        System.out.println(System.currentTimeMillis() - timeLink);
+    }
+}
+
+class MyCollection implements Collection {
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+}
+
+class MyList implements List {
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public Object set(int index, Object element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Object element) {
+
+    }
+
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+}
